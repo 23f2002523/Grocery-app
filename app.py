c153f9479a5457c7dd1a6813754bc988a2a5c467
Northmind04 <23f2002523@ds.study.iitm.ac.in>
2025-07-16 01:10:55 +0530

    made some template files

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 import config # config.py se configuration load karega
 import routes  # routes.py se routes import karega
+import models  # models.py se database models import karega
   
 if __name__ == '__main__':
     app.run(debug=True)
--- routes.py
+++ routes.py
@@ -5,3 +5,10 @@ from app import app  # app.py se Flask instance import karega
 def home():
     return render_template('index.html')  # templates/index.html render karega
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
